3b3df211aee845a482e0dcdbb413c93dedc70344
ndokmai <nd2ga@virginia.edu>
2014-05-05 20:50:51 -0400

    The library can be tested in this file.

--- main.rs
+++ main.rs
@@ -1,49 +1,48 @@
 extern crate time;
-extern crate rand;
 extern crate nonblockinghashmap;
+extern crate rand;
 use	nonblockinghashmap::{NonBlockingHashMap, print_all};
+//mod nonblockinghashmap;
+//mod kvtable;
+//mod keyvalue;
 
 #[allow(unused_unsafe)]
+#[allow(unused_variable)]
 fn main(){
+	let newmap = NonBlockingHashMap::<~str,~str>::new();
+	let shared_map = std::sync::arc::UnsafeArc::new(newmap);
+	let nthreads = 20;
+	let put = 100;
+	let get = 1000;
+
 	unsafe {
-		let newmap = NonBlockingHashMap::<~str,~str>::new();
-		let shared_map = std::sync::arc::UnsafeArc::new(newmap);
 		let (noti_chan, noti_recv) = std::comm::channel();
-		let mut r = rand::task_rng();
-		let nthreads = 1;
-		let rwnum = 10000;
-		//let writeinitial = rwum>>2;
-
-		let readpercent = 99;
-		//let readcnt =
-
 		for n in range(0, nthreads){
-			let child_map = shared_map.clone();
-			let noti_chan_clone = noti_chan.clone();
+			let child_map_put = shared_map.clone();
+			let child_map_get = shared_map.clone();
+			let noti_chan_clone_put = noti_chan.clone();
+			let noti_chan_clone_get = noti_chan.clone();
+
+
 			spawn( proc() {
-				for i in range(0, 100){
-					(*child_map.get()).put("key"+i.to_str(), n.to_str()+"_value"+i.to_str());
+				for i in range(0, put){
+					(*child_map_put.get()).put("key"+i.to_str(),"value"+i.to_str()+"_t"+n.to_str());
 				}
-				noti_chan_clone.send(());
+				noti_chan_clone_put.send(());
+			} );
+
+			spawn( proc() {
+				for i in range(0, get){
+					let key ="key"+(i%put).to_str();
+					println!("(key, value) = ({}, {})", key.clone(), (*child_map_get.get()).get(key));
+				}
+				noti_chan_clone_get.send(());
 			} );
 
 		}
-		for _ in range(0, nthreads){
+		for _ in range(0, nthreads*2){
 			noti_recv.recv();	
 		}
-		print_all(&(*shared_map.clone().get()));
-		let mut reader = std::io::stdio::stdin();
-		loop {
-			print!("Key: ");
-			let input: ~str = reader.read_line().unwrap().trim().to_owned();
-			println!("Value: {}", (*shared_map.get()).get(input));
-			
-		}
-		//newmap.help_copy();
-		//print_all(&newmap);
-
-		//newmap.help_copy();
-		//print_all(&newmap);
 	}
 }
 
